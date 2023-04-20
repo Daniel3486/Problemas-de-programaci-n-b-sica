50e732ba8d823488e8c598f097d52438da1f4858
Daniel <a348603@uach.mx>
2023-04-19 19:52:55 -0600

    Crea un arreglo de 20 elementos con nombres de personas. Muestra los elementos de la lista cada uno en una fila distinta.

---
+++ 32.js
@@ -0,0 +1,3 @@
+let arreglo = ["Juan", "María", "Pedro", "José", "Rosa", "Luis", "Ana", "Miguel", "Carmen", "Francisco", "Guadalupe", "Jorge", "Alejandro", "Manuel", "Patricia", "Carlos", "Adriana", "Fernando", "Lucía", "Daniel"];
+for(let i of arreglo)
+	console.log(i);
